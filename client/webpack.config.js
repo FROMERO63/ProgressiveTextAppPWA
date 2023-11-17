b7a86bfb85d90fc98ad68483fe74de8b89482ef6
FROMERO63 <romerofrank63@gmail.com>
2023-11-16 22:40:39 -0800

    added in the info to the webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,60 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title:"JATE",
+      }),
+      //Added in InjectManifest
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      //creates manifest.json
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Progressive Text App",
+        short_name: "PWA",
+        description: "Progressive text editor with indexedDB",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            //LOGO
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
       
     ],
 
     module: {
       rules: [
+        //bundles the css files
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        //bundles the js files
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        }
         
       ],
     },
